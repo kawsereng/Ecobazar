208a645e19e0dbca16f0ea5afbc652aba5f9baab
MD KAWSER MIA <iamkawsermia@gmail.com>
2026-04-22 21:20:48 +0600

    All page Creat Done

---
+++ src/pages/Cheakout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Cheakout = () => {
+  return (
+    <div>Cheakout</div>
+  )
+}
---
+++ src/pages/DashBord/UserDashbord.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const UserDashbord = () => {
+  return (
+    <div>UserDashbord</div>
+  )
+}
+
